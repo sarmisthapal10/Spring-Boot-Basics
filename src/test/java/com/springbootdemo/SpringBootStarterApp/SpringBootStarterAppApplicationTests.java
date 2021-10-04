3547762e658e6803a31ed10e20989c9f12e01767
SARMISTHA PAL <sarmisthapal1127@gmail.com>
2021-10-04 23:27:03 +0530

    Initial Setup

---
+++ src/test/java/com/springbootdemo/SpringBootStarterApp/SpringBootStarterAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootdemo.SpringBootStarterApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStarterAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
