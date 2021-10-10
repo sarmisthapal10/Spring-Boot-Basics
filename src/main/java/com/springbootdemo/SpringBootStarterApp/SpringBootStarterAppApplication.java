bbc136b00329b8a2073752a6de4c58f46e19866d
SARMISTHA PAL <sarmisthapal1127@gmail.com>
2021-10-10 13:53:31 +0530

    Basic Bean creation example added

--- src/main/java/com/springbootdemo/SpringBootStarterApp/SpringBootStarterAppApplication.java
+++ src/main/java/com/springbootdemo/SpringBootStarterApp/SpringBootStarterAppApplication.java
@@ -1,13 +1,31 @@
 package com.springbootdemo.SpringBootStarterApp;
 
+import org.apache.catalina.core.ApplicationContext;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootStarterAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootStarterAppApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+
+		};
 	}
 
 }
